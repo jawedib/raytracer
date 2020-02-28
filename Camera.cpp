c52cab92a182fdbcaee741d049732221d1d15a3b
unknown <jahsan0594@gmail.com>
2020-02-27 21:55:49 -0500

    refactored folders

--- Camera.cpp
+++
@@ -1,8 +0,0 @@
-#include "Camera.h"
-#include <iostream>
-
-Camera::Camera(glm::vec3 pos, float fov, float focal_length, float aspect_ratio) :
-	pos(pos), fov(fov), focal_length(focal_length), aspect_ratio(aspect_ratio)
-{
-	//std::cout << "fov: " + std::to_string(fov) + "\n";
-}
--- Light.cpp
+++
@@ -1,7 +0,0 @@
-#include "Light.h"
-
-
-Light::Light(glm::vec3 position, glm::vec3 color) :
-	position(position), color(color)
-{
-}
